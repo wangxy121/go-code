8fe86cb752ec0df6b89a4ed78177b26817cbd306
wangxinyu1 <wangxinyu1@qeeka.com>
2020-09-14 19:51:45 +0800

    fix

--- interface_uage/demo3/main.go
+++ interface_uage/demo3/main.go
@@ -1,1 +1,24 @@
-package demo3
+package main
+
+import "fmt"
+
+type mover interface {
+	move()
+}
+type person struct {
+	name string
+	age uint8
+}
+
+func (p person) move()  {
+	fmt.Printf("%s在玩",p.name)
+}
+func main() {
+	var x mover
+	p := person{
+		name: "小猫",
+		age: 9,
+	}
+	x = p
+	x.move()
+}
