57e0a00600f6bac8d627a6ad45b17acd421955e5
wangxinyu1 <wangxinyu1@qeeka.com>
2020-09-30 18:08:23 +0800

    add

---
+++ mysql_usage/demo5/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var db *sqlx.DB
+
+func initDB() (err error) {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/sql_test?charset=utf8mb4&parseTime=True"
+	// 也可以使用MustConnect连接不成功就panic
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("connect DB failed, err:%v\n", err)
+		return
+	}
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(10)
+	return
+}
+
+
+func main()  {
+	initDB()
+}
